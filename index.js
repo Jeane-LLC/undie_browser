bba7299daaf8631da36d8765778bc759698bd2e1
Arjun Iyer <arjun.iyer@jeane.llc>
2021-03-16 18:58:58 -0400

    js: button click event; html: align-text, passwordInput, etc

---
+++ index.js
@@ -0,0 +1,8 @@
+var passwordInput = document.getElementById("passwordInput");
+var submitPasswordButton = document.getElementById("submitPasswordButton");
+
+submitPasswordButton.addEventListener("click", onSubmitPasswordbuttonClick);
+
+function onSubmitPasswordButtonClick(event){
+    
+}
