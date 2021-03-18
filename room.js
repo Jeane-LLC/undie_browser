9d1126db9cfdcc9da8662c5ed6f9f8243f2124b9
Arjun Iyer <arjun.iyer@jeane.llc>
2021-03-18 11:17:03 -0400

    billing.js and map control removal

--- room.js
+++ room.js
@@ -1,4 +1,6 @@
 var roomMap = L.map('leaflet').setView([0.0,0.0],13);
+roomMap.attributionControl = false;
+roomMap.zoomControl = false;
 L.tileLayer('').addTo(roomMap);
 
 function onMapClick(event){
