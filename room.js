f43fab92bec5b32816134210263b0fc098d66a5a
Arjun Iyer <arjun.iyer@jeane.llc>
2021-03-18 11:22:04 -0400

    map initialization options

--- room.js
+++ room.js
@@ -1,6 +1,11 @@
-var roomMap = L.map('leaflet').setView([0.0,0.0],13);
-roomMap.attributionControl = false;
-roomMap.zoomControl = false;
+var roomMap = L.map('leaflet',{
+    center: [0.0,0.0],
+    zoom:13,
+    attributionControl:false,
+    zoomControl: false;
+});
+
+
 L.tileLayer('').addTo(roomMap);
 
 function onMapClick(event){
