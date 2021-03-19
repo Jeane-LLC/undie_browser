7fc19a357a483e4d7d738482414d577f1df35cc9
Arjun Iyer <arjun.iyer@jeane.llc>
2021-03-19 17:05:32 -0400

    svg controller attempt

--- room.js
+++ room.js
@@ -1,3 +1,17 @@
 const roomMap = document.getElementById("roomMap")
 const ctx = roomMap.getContext('2d');
-ctx.fillStyle='black';
+
+
+window.addEventListener('resize',render)
+
+function render(){
+    const maxWidth = window.innerWidth;
+    const maxHeight = window.innerHeight;
+    const longestDimension = maxWidth > maxHeight ? maxWidth : maxHeight;
+    const shorterDimension = longestDimension === maxWidth ? maxHeight : maxWidth;
+    const squareness = longestDimension / shorterDimension;
+    const sideLength = shorterDimension * 0.85;
+    const padding = shorterDimension - sideLength;
+    
+    
+}
