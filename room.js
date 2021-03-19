c0e0fb4eb1794a0a7cd7f9ec5c28b6670057e408
Arjun Iyer <arjun.iyer@jeane.llc>
2021-03-19 18:55:11 -0400

    ctx.canvas render

--- room.js
+++ room.js
@@ -5,13 +5,8 @@ const ctx = roomMap.getContext('2d');
 window.addEventListener('resize',render)
 
 function render(){
-    const maxWidth = window.innerWidth;
-    const maxHeight = window.innerHeight;
-    const longestDimension = maxWidth > maxHeight ? maxWidth : maxHeight;
-    const shorterDimension = longestDimension === maxWidth ? maxHeight : maxWidth;
-    const squareness = longestDimension / shorterDimension;
-    const sideLength = shorterDimension * 0.85;
-    const padding = shorterDimension - sideLength;
-    
-    
+    ctx.canvas.width = window.innerwidth;
+    ctx.canvas.height = window.innerHeight;
 }
+
+render();
