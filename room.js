cacf8e532ecc05d78756e2d09b6870559bb87c2f
Arjun Iyer <arjun.iyer@jeane.llc>
2021-03-20 13:18:27 -0400

    simplify it even more please

--- room.js
+++ room.js
@@ -1,11 +1,17 @@
 const roomMap = document.getElementById("roomMap")
 const ctx = roomMap.getContext('2d');
 
+
 function draw(){
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
-
-
+    ctx.beginPath();
+    ctx.arc(window.innerWidth/2,window.innerHeight/2, 35, 0, 2*Math.PI,1);
+    ctx.fillStyle = "red";
+    ctx.strokeStyle = "blue";
+    ctx.stroke();
+    ctx.fill();
+    
     ctx.globalCompositeOperation='destination-over';
     ctx.fillStyle = "grey";
     ctx.fillRect(0,0, ctx.canvas.width,ctx.canvas.height);
