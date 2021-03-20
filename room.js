6e4494c17f06bc90798f167be032df28d8e7923b
Arjun Iyer <arjun.iyer@jeane.llc>
2021-03-20 14:40:15 -0400

    extra-simplified mouse following navigation

--- room.js
+++ room.js
@@ -1,19 +1,26 @@
 const roomMap = document.getElementById("roomMap")
 const ctx = roomMap.getContext('2d');
+roomMap.addEventListener('click',move)
+var me = {x:window.innerWidth/2,y:window.innerHeight/2,theta:3*Math.PI/2};
 
-
+function move(event){
+    me.theta = Math.atan2(event.clientY-me.y,event.clientX-me.x);
+    me.x = event.clientX;
+    me.y = event.clientY;
+}
 function draw(){
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
     ctx.beginPath();
-    ctx.arc(window.innerWidth/2,window.innerHeight/2, 35, 0, 2*Math.PI,1);
+    ctx.arc(me.x,me.y, 20, 0, 2*Math.PI,1);
     ctx.fillStyle = "red";
     ctx.strokeStyle = "blue";
-    ctx.stroke();
     ctx.fill();
-    
+    ctx.beginPath();
+    ctx.arc(me.x,me.y,25,me.theta+Math.PI/2,me.theta-Math.PI/2,1);
+    ctx.stroke();
     ctx.globalCompositeOperation='destination-over';
-    ctx.fillStyle = "grey";
+    ctx.fillStyle = "white";
     ctx.fillRect(0,0, ctx.canvas.width,ctx.canvas.height);
 
     window.requestAnimationFrame(draw);
